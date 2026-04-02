50f64c3b755961c795724acd97686a53f573cadd
Aaron Cole <cole.1214@osu.edu>
2026-04-02 15:19:48 -0400

    Set up basic MainMenu scene, and created file UIManager to deal with UI things.

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Restart()
+    {
+        //Restart game scene
+    }
+
+    void Exit()
+    {
+        //Exit the game
+    }
+
+    void LoadScene()
+    {
+        //Used to load between MainMenu, the Main game scene, and Tutorial scene (if we make one).
+    }
+}
