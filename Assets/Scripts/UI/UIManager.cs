6f18276d0be1130c2c535b009214e42d1282044b
Aaron Cole <cole.1214@osu.edu>
2026-04-07 14:12:46 -0400

    Added Scene changing funcitonality to UIManager and started adding support for a debug menu. Also created some Gameplay UI features as proof of concept.

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    private static bool GameIsPaused = false;
+    private static bool UIOpen = false;
+
+    [Header("UI References")]
+    public GameObject endOfDayUI;
+    public GameObject debugMenu;
+    
+    [SerializeField]
+    public string mainMenu;
+    public string gameSceneName;
     // Start is called before the first frame update
     void Start()
     {
-        
+       Time.timeScale = 1f;
+       GameIsPaused = false;
+       UIOpen = false;
+ 
     }
 
     // Update is called once per frame
@@ -19,15 +34,47 @@ public class UIManager : MonoBehaviour
     void Restart()
     {
         //Restart game scene
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(gameSceneName);
+        }
     }
 
     void Exit()
     {
         //Exit the game
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 
-    void LoadScene()
+    void LoadGameScene()
     {
         //Used to load between MainMenu, the Main game scene, and Tutorial scene (if we make one).
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    void LoadMainMenuScene()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+
+    void OpenDebug()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            debugMenu.SetActive(true);
+        }
+        while (debugMenu == true)
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                GameIsPaused = true;
+                Time.timeScale = 0f;
+            }
+        }
+        Time.timeScale = 1f;
+        GameIsPaused = false;
     }
 }
